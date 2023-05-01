d50851ad634366a910341e3737c86281935e6ec6
AliMohamed <shigamotowaeto@gmail.com>
2023-05-01 23:11:53 +0300

    modified level structure

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@
 using namespace std;
 using namespace sf;
 
-void construct(Sprite item[], int start, int end, int amount, float x, int differenceX, int differenceY, float y)
+void construct(Sprite item[], int start, int amount, float x, int differenceX, int differenceY, float y)
 {
-    for (int i = start, j = end; i < amount; i++, j++)
-       item[i].setPosition(Vector2f(x + (j * differenceX), y - (j * differenceY)));
+    for (int i = start, j = 0; i < amount; i++, j++)
+        item[i].setPosition(Vector2f(x + (j * differenceX), y - (j * differenceY)));
 }
 
 void setGround(Sprite item[], int num, int width, int height, int x, int y)
 {
-    item[num].setTextureRect(IntRect(0, 0, width, height)); 
+    item[num].setTextureRect(IntRect(0, 0, width, height));
     item[num].setPosition(x, y);
 }
 
@@ -66,62 +66,86 @@ int main()
     Vector2f Velocity;
 
     //Textures
-    Texture spiketex;
-    Texture platformtex;
-    Texture Groundtex;
+    Texture spikeTexture;
+    Texture platformTexture;
     Texture backgroundTexture;
     Texture groundTexture;
     Texture ringTexture;
     Texture rockTexture;
     Texture waterTexture;
+    Texture treeTexture;
 
     // Loading From Files
     sonic.sonicTex.loadFromFile("sonic22.png");
-    platformtex.loadFromFile("Wall.png");
-    Groundtex.loadFromFile("Wall.png");
-    spiketex.loadFromFile("Spike.png");
+    platformTexture.loadFromFile("Wall.png");
+    spikeTexture.loadFromFile("Spike.png");
     backgroundTexture.loadFromFile("background.png");
     groundTexture.loadFromFile("ground2.png");
     ringTexture.loadFromFile("ring.png");
     rockTexture.loadFromFile("rock.png");
     waterTexture.loadFromFile("water.png");
+    treeTexture.loadFromFile("tree.png");
 
     // Sprites
     Sprite platform[20];
-    Sprite Ground;
-    Sprite spike[20];
+    Sprite spike[50];
     Sprite ring[100];
+    Sprite tree[100];
+    Sprite rock[2];
 
     // Setting Textures
     sonic.player.setTexture(sonic.sonicTex);
     for (int i = 0; i < 20; i++)
-        platform[i].setTexture(platformtex);
-    Ground.setTexture(Groundtex);
-    for (int i = 0; i < 20; i++)
-        spike[i].setTexture(spiketex);
+        platform[i].setTexture(platformTexture);
+    for (int i = 0; i < 50; i++)
+        spike[i].setTexture(spikeTexture);
     for (int i = 0; i < 100; i++)
         ring[i].setTexture(ringTexture);
-    
+    for (int i = 0; i < 100; i++)
+        tree[i].setTexture(treeTexture);
+    for (int i = 0; i < 2; i++)
+        rock[i].setTexture(rockTexture);
+
     // Building
-    construct(ring, 0, 0, 11, 960, 50, 0, 890);
-    construct(ring, 11, 0, 22, 960, 50, 0, 828);
-    construct(spike, 0, 0, 3, 2000, 200, 0, 856);
-    construct(platform, 0, 0, 2, 2700, 400, 0, 650);
-    construct(ring, 22, 0, 24, 2767, 400, 0, 590);
-    construct(spike, 3, 0, 6, 3527, 200, 0, 856);
-    construct(ring, 22, 0, 33, 4474, 50, 0, 890);
-    construct(ring, 33, 0, 44, 4474, 50, 0, 828);
-    construct(platform, 2, 0, 5, 6500, 350, 150, 828);
-    construct(spike, 6, 0, 9, 6630, 350, 150, 735);
-    construct(ring, 44, 0, 47, 6550, 350, 150, 770);
-    construct(ring, 47, 0, 66, 7500, 150, 0, 890);
-    construct(spike, 9, 0, 19, 7570, 300, 0, 856);
+    construct(ring, 0, 11, 960, 50, 0, 890);
+    construct(ring, 11, 22, 960, 50, 0, 828);
+    construct(spike, 0, 3, 2000, 200, 0, 856);
+    construct(platform, 0, 2, 2700, 400, 0, 650);
+    construct(ring, 22, 24, 2767, 400, 0, 590);
+    construct(spike, 3, 6, 3527, 200, 0, 856);
+    construct(ring, 24, 35, 4474, 50, 0, 890);
+    construct(ring, 35, 46, 4474, 50, 0, 828);
+    construct(platform, 2, 5, 6500, 350, 150, 828);
+    construct(spike, 6, 9, 6630, 350, 150, 735);
+    construct(ring, 46, 49, 6550, 350, 150, 770);
+    construct(ring, 49, 60, 7500, 150, 0, 890);
+    construct(spike, 9, 15, 7570, 300, 0, 856);
+    construct(tree, 0, 2, 100, 1500, 0, 238);
+    construct(tree, 2, 4, 3300, 1700, 0, 238);
+    construct(tree, 4, 7, 6000, 1613, 0, 238);
+    construct(platform, 5, 10, 11700, 500, 0, 700);
+    construct(ring, 60, 64, 11880, 50, 60, 470);
+    construct(ring, 64, 67, 12080, 50, -60, 350);
+    construct(ring, 67, 71, 12380, 50, 60, 470);
+    construct(ring, 71, 74, 12580, 50, -60, 350);
+    construct(ring, 74, 78, 12880, 50, 60, 470);
+    construct(ring, 78, 81, 13080, 50, -60, 350);
+    construct(ring, 81, 85, 13380, 50, 60, 470);
+    construct(ring, 85, 88, 13580, 50, -60, 350);
+    construct(tree, 7, 9, 11200, 3000, 0, 238);
+    construct(spike, 15, 19, 14600, 64, 0, 856);
+    construct(spike, 19, 23, 15400, 64, 0, 856);
+    construct(spike, 23, 27, 16200, 64, 0, 856);
+    construct(spike, 27, 31, 17000, 64, 0, 856);  
+    construct(spike, 31, 35, 17800, 64, 0, 856);
+    construct(platform, 10, 14, 15030, 800, 0, 700);
+    construct(ring, 88, 92, 15096, 800, 0, 640);
+    construct(tree, 9, 11, 15600, 2000, 0, 238);
+    construct(rock, 0, 1, -690, 0, 0, 543);
+    construct(rock, 1, 2, -905, 0, 0, 747);
 
     // Sonic Position
-    sonic.player.setPosition(350, 700);
-
-    // Ground Position
-    Ground.setPosition(200, 800);
+    sonic.player.setPosition(250, 720);
 
     // Scaling
     sonic.player.setScale(Vector2f(2.f, 2.f));
@@ -129,10 +153,12 @@ int main()
     sonic.LeftColl.setSize(Vector2f(15.f, 70.f));
     sonic.RightColl.setSize(Vector2f(15.f, 70.f));
     for (int i = 0; i < 20; i++)
-    platform[i].setScale(1.6, 1);
-    Ground.setScale(11.9, 2);
-    for (int i = 0; i < 20; i++)
-    spike[i].setScale(0.5, 0.6);
+        platform[i].setScale(1.6, 1);
+    for (int i = 0; i < 50; i++)
+        spike[i].setScale(0.5, 0.6);
+    for (int i = 0; i < 100; i++)
+        tree[i].setScale(2, 2);
+    rock[1].setScale(0.5, 0.5);
 
     // Background
     Sprite background;
@@ -140,16 +166,7 @@ int main()
     background.setTextureRect(IntRect(0, 0, 1920 * 33.33333, 1080));
     background.setPosition(Vector2f(-1000, 0));
     backgroundTexture.setRepeated(15);
-
-    // Rocks
-    Sprite rock1;
-    Sprite rock3;
-    rock1.setTexture(rockTexture);
-    rock3.setTexture(rockTexture);
-    rock1.setPosition(-690, 543);
-    rock3.setPosition(-905, 747);
-    rock3.setScale(0.5, 0.5);
-
+ 
     // Border
     RectangleShape border1(Vector2f(10, 1080));
     border1.setFillColor(Color::Transparent);
@@ -167,21 +184,21 @@ int main()
     setGround(water, 0, 800, 128, 2600, 980);
     setGround(ground, 2, 4000, 128, 7500, 952);
     setGround(water, 1, 1500, 128, 6100, 980);
+    setGround(water, 2, 2600, 128, 11500, 980);
+    setGround(ground, 3, 9000, 128, 14100, 952); // last ground (width: 9000)
     groundTexture.setRepeated(15);
     waterTexture.setRepeated(15);
 
     // Fonts
     int score = 0;
     Font font;
-    font.loadFromFile("font2.ttf");
+    font.loadFromFile("font.ttf");
     Text collector;
     collector.setFont(font);
     collector.setString("Score: " + to_string(score));
-    collector.setPosition(-600, 100);
+    collector.setPosition(-680, 100);
     collector.setCharacterSize(72);
     collector.setFillColor(Color::White);
-
-  
 
     // Sounds
     SoundBuffer coinsoundbuffer;
@@ -219,7 +236,7 @@ int main()
                     sonic.Velocity.y = -30;
                     isground = false;
                     jumpsound.play();
-                    cout << sonic.player.getPosition().x << endl;
+
                 }
 
 
@@ -256,18 +273,9 @@ int main()
                 //if sonic on the ground:
                 if (sonic.player.getGlobalBounds().intersects(ground[i].getGlobalBounds()))
                 {
-                    //if not:
-                    if ((sonic.player.getPosition().x >= ground[i].getPosition().x + 64000)) {}
-                    else if ((sonic.PlayerColl.getPosition().x + 40 <= ground[i].getPosition().x)) {}
-
-                    //then:
-                    else
-                    {
-                        sonic.player.setPosition(sonic.player.getPosition().x, ground[i].getPosition().y - 75);
-                        sonic.Velocity.y = -0.01;
-                        isground = true;
-                    }
-
+                    sonic.player.setPosition(sonic.player.getPosition().x, ground[i].getPosition().y - 75);
+                    sonic.Velocity.y = -0.01;
+                    isground = true;
                 }
                 //if sonic is above the ground:
                 else
@@ -384,7 +392,7 @@ int main()
             }
 
 
-            if (Keyboard::isKeyPressed(Keyboard::D))
+            else if (Keyboard::isKeyPressed(Keyboard::D))
             {
                 sonic.Velocity.x = 8 + acc;
                 Velocity.x = 8 + acc;
@@ -442,8 +450,19 @@ int main()
                 else
                     timer -= deltatime;
             }
-            
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < 3; i++)
+            {
+                if (sonic.PlayerColl.getGlobalBounds().intersects(water[i].getGlobalBounds()))
+                {
+                    isground = false;
+                    sonic.Velocity.y = -0.01;
+                    sonic.player.setPosition(sonic.player.getPosition().x, sonic.player.getPosition().y - 200);
+                    isdead = true;
+                    deathsound.play();
+                }
+            }
+
+            for (int i = 0; i < 50; i++)
             {
                 if (sonic.PlayerColl.getGlobalBounds().intersects(spike[i].getGlobalBounds()) && isground)
                 {
@@ -478,8 +497,10 @@ int main()
             }
 
         }
+
         if (isdead)
         {
+
 
             for (int deadanim = 6; deadanim <= 7; deadanim++)
             {
@@ -499,8 +520,8 @@ int main()
         }
         if (sonic.player.getGlobalBounds().intersects(border1.getGlobalBounds()))
         {
-            sonic.player.setPosition(sonic.player.getPosition().x + 15, sonic.player.getPosition().y);
-            collector.setPosition(collector.getPosition().x + 15, collector.getPosition().y);
+            sonic.player.setPosition(sonic.player.getPosition().x - sonic.Velocity.x, sonic.player.getPosition().y);
+            collector.setPosition(collector.getPosition().x - Velocity.x, collector.getPosition().y);
         }
 
         //Rings Disappearing When Collision
@@ -509,13 +530,10 @@ int main()
             if (sonic.player.getGlobalBounds().intersects(ring[i].getGlobalBounds()) && !isdead)
             {
                 ring[i].setScale(Vector2f(0, 0));
-                score++;
+                score += 100;
                 coinsound.play();
             }
         }
-
-
-
 
         //Rings Animation
         ringanimator++;
@@ -526,12 +544,11 @@ int main()
         }
         if (sonic.PlayerColl.getPosition().y > window.getSize().y)
         {
-            sonic.player.setPosition(300, 720);
-            collector.setPosition(-600, 100);
             isdead = false;
             isground = true;
+            sonic.player.setPosition(250, 720);
+            collector.setPosition(-680, 100);
         }
-
 
         collector.setString("Score: " + to_string(score));
 
@@ -546,26 +563,27 @@ int main()
         //Draw
         window.draw(collector);
         window.draw(background);
-        for (int i = 0; i < 2; i++)
-            window.draw(water[i]);
+        for (int i = 0; i < 11; i++)
+        window.draw(tree[i]);
         for (int i = 0; i < 3; i++)
-        window.draw(ground[i]);
+            window.draw(water[i]);
+        for (int i = 0; i < 4; i++)
+            window.draw(ground[i]);
         window.draw(border1);
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 92; i++)
             window.draw(ring[i]);
-        window.draw(rock1);
-        window.draw(rock3);
+        for (int i = 0; i < 2; i++)
+            window.draw(rock[i]);
         window.draw(sonic.player);
         window.draw(collector);
-        for (int i = 0; i < 20; i++)
-        window.draw(platform[i]);
-        for (int i = 0; i < 20; i++)
-        window.draw(spike[i]);
+        for (int i = 0; i < 14; i++)
+            window.draw(platform[i]);
+        for (int i = 0; i < 35; i++)
+            window.draw(spike[i]);
         window.draw(collector);
 
         //Display
         window.display();
         deltatime = gameclock.getElapsedTime().asSeconds();
-
     }
 }
